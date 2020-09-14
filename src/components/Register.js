7b3ef4b8815d783ac212c1cc32d7724558ddd387
Jukkrid Boonklom <choteseeworld@gmail.com>
2020-09-14 23:39:49 +0700

    prepare ci/cd

--- src/components/Register.js
+++ src/components/Register.js
@@ -61,6 +61,7 @@ export default function Register() {
         ) : null}
 
         <strong>ชื่อห้องที่จะขอรับการแจ้งเตือน Line Notify</strong>
+        <strong>ชื่อห้องที่จะขอรับการแจ้งเตือน Line Notify</strong>
         <form className={classes.root} autoComplete="off">
           <CssTextField
             required
