d090a812edb1d356c1d71b688df2339397f03615
Jukkrid Boonklom <choteseeworld@gmail.com>
2020-09-18 20:27:09 +0700

    again build

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,12 +1,11 @@
-// import React, { useState, useEffect } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
-// import { GROUPNAME, COMPANYNAME } from "../Auth";
+import { GROUPNAME, COMPANYNAME } from "../Auth";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-// import axios from "axios";
-// import querystring from "querystring";
+import axios from "axios";
+import querystring from "querystring";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,43 +19,43 @@ const useStyles = makeStyles((theme) => ({
 export default function Callback() {
   const classes = useStyles();
 
-  // const [groupName, setGroupName] = useState("");
-  // const [companyName, setCompanyName] = useState("");
+  const [groupName, setGroupName] = useState("");
+  const [companyName, setCompanyName] = useState("");
 
-  // const [code, setCode] = useState("");
+  const [code, setCode] = useState("");
 
-  // const getToken = () => {
-  //   axios
-  //     .post(
-  //       "http://localhost:5000/code",
-  //       querystring.stringify({
-  //         code: code,
-  //         groupName: groupName,
-  //         companyName: companyName,
-  //       }),
-  //       { headers: { "content-type": "application/x-www-form-urlencoded" } }
-  //     )
-  //     .then(() => {
-  //       // console.log("response: ", response);
+  const getToken = () => {
+    axios
+      .post(
+        "http://localhost:5000/code",
+        querystring.stringify({
+          code: code,
+          groupName: groupName,
+          companyName: companyName,
+        }),
+        { headers: { "content-type": "application/x-www-form-urlencoded" } }
+      )
+      .then(() => {
+        // console.log("response: ", response);
 
-  //       localStorage.clear();
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
+        localStorage.clear();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  // useEffect(() => {
-  //   // console.log("Do in use effect!!!");
-  //   const urlParams = new URLSearchParams(window.location.search);
-  //   const myParam = urlParams.get("code");
+  useEffect(() => {
+    // console.log("Do in use effect!!!");
+    const urlParams = new URLSearchParams(window.location.search);
+    const myParam = urlParams.get("code");
 
-  //   setGroupName(localStorage.getItem(GROUPNAME));
-  //   setCompanyName(localStorage.getItem(COMPANYNAME));
-  //   setCode(myParam);
+    setGroupName(localStorage.getItem(GROUPNAME));
+    setCompanyName(localStorage.getItem(COMPANYNAME));
+    setCode(myParam);
 
-  //   getToken();
-  // }, []);
+    getToken();
+  }, []);
 
   return (
     <div className="App">
@@ -66,9 +65,9 @@ export default function Callback() {
             ท่านได้ทำการลงทะเบียนไลน์ การแจ้งเตือนผ่าน Line Notify เรียบร้อยแล้ว
           </strong>
           <br></br>
-          {/* <strong>
+          <strong>
             {groupName} {companyName}
-          </strong> */}
+          </strong>
 
           <strong></strong>
         </div>
